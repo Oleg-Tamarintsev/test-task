29720bc573405188a4a56cb7f8f1e4091483c3ee
Oleg Tamarintsev <oleg-tamarintsev@ya.ru>
2017-11-22 16:40:06 +0700

    Add starting point

---
+++ SimpleFileSystemElements/Source/SimpleFileSystem/Path.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleFileSystem
+{
+    using System;
+
+    public sealed class Path
+    {
+        public Path(string inputPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cd(string inputPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CurrentPath
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ SimpleFileSystemElements/Tests/SimpleFileSystemTests/PathTests.cs
@@ -0,0 +1,30 @@
+﻿namespace SimpleFileSystemTests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using SimpleFileSystem;
+
+    [TestFixture]
+
+    public sealed class PathTests
+    {
+        [TestCase("/", "/", TestName = "Path should be initialized with root path")]
+        public void Path_Should_Be_Initialized(string inputPath, string outputPath)
+        {
+            var path = new Path(inputPath);
+
+            path.CurrentPath.Should().Be(outputPath);
+        }
+
+        [Ignore("Not now!")]
+        [TestCase("/a/b/c/d", "../x", "/a/b/c/x", TestName = "Cd method should move path to other subdirectory (Acceptance Test)")]
+        public void Cd_Method_Should_Move_Path(string initialPath, string relatitivePath, string outputPath)
+        {
+            Path path = new Path(initialPath);
+
+            path.Cd(relatitivePath);
+
+            path.CurrentPath.Should().Be(outputPath);
+        }
+    }
+}
