7b260bce77147a099be0d2575f9331245f9037f6
Oleg Tamarintsev <oleg-tamarintsev@ya.ru>
2017-11-22 16:44:04 +0700

    stupid code: first test passed (TDD)

--- SimpleFileSystemElements/Source/SimpleFileSystem/Path.cs
+++ SimpleFileSystemElements/Source/SimpleFileSystem/Path.cs
@@ -1,12 +1,16 @@
 ﻿namespace SimpleFileSystem
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public sealed class Path
     {
+        private List<string> _subdirectories;
+
         public Path(string inputPath)
         {
-            throw new NotImplementedException();
+            _subdirectories = inputPath.Split('/').Skip(1).ToList();
         }
 
         public void Cd(string inputPath)
@@ -18,7 +22,13 @@
         {
             get
             {
-                throw new NotImplementedException();
+                var sb = new System.Text.StringBuilder();
+                foreach (var item in _subdirectories)
+                {
+                    sb.Append('/');
+                    sb.Append(item);
+                }
+                return sb.ToString();
             }
         }
     }
